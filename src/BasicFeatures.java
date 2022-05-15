b1b98f5e94c537971c3f8bd1861e2170010c7319
Michael <fallen0331@gmail.com>
2022-05-15 11:03:21 -0400

    the basic structure of the car is ready, working on advance features

---
+++ src/BasicFeatures.java
@@ -0,0 +1,10 @@
+public class BasicFeatures {
+
+    public class basicFeatures {
+        String engineCapac;
+        String Color;
+        String doors;
+        String headlights;
+        String regNo;
+    }
+}
